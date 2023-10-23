ab35c4b95825bf10fef49f7b6d653d9f9884f0d0
Ajay Kumar Sarkar <ajsarkar@deloitte.com>
2023-10-23 21:17:25 +0530

    Add project files.

---
+++ NZWalks.API/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            string[] studentArray = { "Suraj", "Sonam", "Ajay" };
+            return Ok(studentArray);
+        }
+    }
+}
